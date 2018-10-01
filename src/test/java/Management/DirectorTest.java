b925b13c418403da81ca4f8dbc087e03a1cd7fde
fddata <fraserjdouglas@gmail.com>
2018-10-01 15:54:47 +0100

    adds director and DirectorTest classes, passes all tests

---
+++ src/test/java/Management/DirectorTest.java
@@ -0,0 +1,53 @@
+package Management;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DirectorTest {
+    Director director;
+
+    @Before
+    public void setUp() throws Exception {
+        director = new Director("Janice", "DEF123", 50000, "IT", 100000);
+    }
+
+
+    @Test
+    public void getDeptName() {
+        assertEquals("IT", director.getDeptName());
+    }
+
+    @Test
+    public void getName() {
+        assertEquals("Janice", director.getName());
+    }
+
+    @Test
+    public void getNiNumber() {
+        assertEquals("DEF123", director.getNiNumber());
+    }
+
+    @Test
+    public void getSalary() {
+        assertEquals(50000, director.getSalary(), 0);
+    }
+
+    @Test
+    public void raiseSalary() {
+        director.raiseSalary(1.5);
+        assertEquals(75000, director.getSalary(), 0);
+    }
+
+    @Test
+    public void payBonus() {
+        assertEquals(500, director.payBonus(), 0);
+    }
+
+
+    @Test
+    public void getBudget() {
+        assertEquals(100000, director.getBudget(), 0);
+    }
+}
