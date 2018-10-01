a39870ed29b4e411689965439851017fe0c45fae
fddata <fraserjdouglas@gmail.com>
2018-10-01 15:33:26 +0100

    creates Employee abstract class

---
+++ src/test/java/Staff/EmployeeTest.java
@@ -0,0 +1,15 @@
+package Staff;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class EmployeeTest {
+
+    Employee employee;
+
+    @Before
+//    public void setUp() throws Exception {
+//        employee = new Employee("Bob", "AB123456C", 25000);
+    }
+}
